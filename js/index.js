31947114ce2e72047cd10f5da41e18f89ebe19fb
bigjang77 <dhex2004@gmail.com>
2023-04-18 13:28:41 +0900

    수정

--- js/index.js
+++ js/index.js
@@ -1,42 +1,52 @@
 const publicKey = 'BMQlFP2pmowjPJGZPy87Iy9qGcm-ICh9bZE5KR9m1VAaO96TKDo5JM_5qyv_ZpGEMh_3fAJW3nEI1taFohKCS-M';
 
-// 구독 버튼 클릭 이벤트
-document.getElementById('subscribes').addEventListener('click', async function () {
-    // Service Worker 등록
-    const registration = await navigator.serviceWorker.register('./sw.js', { scope: '/' });
-    console.log('Service Worker registered: ', registration);
-
-    // Push Notification 등록
-    const subscription = await registration.pushManager.subscribe({
-        userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(publicKey),
+//서비스워커 등록
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker
+            .register('./sw.js')
+            .then(reg => console.log('Service Worker: Registered (Pages)'))
+            .catch(err => console.log(`Service Worker: Error: ${err}`));
     });
-    console.log('Push subscription: ', subscription);
+}
 
-    // 알림 전송
-    await fetch('/subscribe', {
-        method: 'POST',
-        body: JSON.stringify(subscription),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
+// 구독 버튼 클릭 이벤트 핸들러
+var subscribeButton = document.getElementById('subscribes');
+subscribeButton.addEventListener('click', function () {
+    if (Notification.permission === 'granted') {
+        navigator.serviceWorker.ready.then(function (registration) {
+            registration.pushManager.subscribe({
+                userVisibleOnly: true,
+                applicationServerKey: urlBase64ToUint8Array(publicKey)
+            }).then(function (subscription) {
+                console.log('User is subscribed.');
 
-    // PWA 알림 전송
-    registration.showNotification('구독 완료!', {
-        body: '성공적으로 구독되었습니다.',
-        icon: './images/notification-icon.png',
-    });
+                // 구독 알림 전송
+                registration.showNotification('🎊구독 알림', {
+                    body: '구독이 완료되었습니다.',
+                    icon: '/images/icon-72x72.png',
+                    badge: '/images/badge-96x96.png'
+                });
+            }).catch(function (err) {
+                console.log('Failed to subscribe the user: ', err);
+            });
+        }).catch(function (err) {
+            console.log('Failed to get service worker registration: ', err);
+        });
+    } else {
+        console.log('Notification permission denied.');
+    }
 });
 
-// base64 URL을 Uint8Array로 변환
+// Public VAPID Key를 Uint8Array로 변환하는 함수
 function urlBase64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
-    const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
-    for (let i = 0; i < rawData.length; ++i) {
+    var padding = '='.repeat((4 - base64String.length % 4) % 4);
+    var base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
+    var rawData = window.atob(base64);
+    var outputArray = new Uint8Array(rawData.length);
+    for (var i = 0; i < rawData.length; ++i) {
         outputArray[i] = rawData.charCodeAt(i);
     }
     return outputArray;
 }
+
--- sw.js
+++ sw.js
@@ -1,3 +1,48 @@
+const CACHE_NAME = 'my-cache';
+const CACHE_URLS = [
+    // '/',
+    // 'styles/styles.css',
+    // 'js/index.js',
+    // 'icons/'
+    // 필요한 리소스들을 캐싱합니다.
+];
+
+self.addEventListener('install', event => {
+    // 서비스워커가 설치되면, 리소스들을 캐싱합니다.
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(CACHE_URLS))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    // 서비스워커가 활성화되면, 이전 버전의 캐시를 정리합니다.
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    // 네트워크 요청을 가로채서 캐시된 리소스를 반환하거나 동적으로 리소스를 캐싱합니다.
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+
+
 self.addEventListener('push', function (event) {
     if (event.data) {
         console.log('Push notification received: ', event.data.text());
